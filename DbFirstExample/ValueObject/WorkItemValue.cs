c3f67a82c8189f58ab39bd0cc6759a8d411016f0
Felaray Ho <felaray@gmail.com>
2024-08-08 15:00:42 +0800

    update WorkItemValue class to record

--- DbFirstExample/ValueObject/WorkItemValue.cs
+++ DbFirstExample/ValueObject/WorkItemValue.cs
@@ -4,20 +4,12 @@ namespace DbFirstExample.ValueObject
 {
     //DDD value object
     [Owned]
-    public class WorkItemValue
+    public record WorkItemValue(int WorkItemId, string Description, int WorkTypeId, string WorkTypeName)
     {
-        public int WorkItemId { get; private set; }
-        public string Description { get; private set; }
-        public int WorkTypeId { get; private set; }
-        public string WorkTypeName { get; private set; }
-
-        public WorkItemValue(int workItemId, string description, int workTypeId, string workTypeName)
-        {
-            WorkItemId = workItemId;
-            Description = description;
-            WorkTypeId = workTypeId;
-            WorkTypeName = workTypeName;
-        }
+        public int WorkItemId { get; private set; } = WorkItemId;
+        public string Description { get; private set; } = Description;
+        public int WorkTypeId { get; private set; } = WorkTypeId;
+        public string WorkTypeName { get; private set; } = WorkTypeName;
 
         //equatable
 
