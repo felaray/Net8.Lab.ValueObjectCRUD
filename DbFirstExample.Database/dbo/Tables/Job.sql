0f366121baab19a1fa2f54e565ad68809f65adde
Felaray Ho <felaray@gmail.com>
2024-08-08 15:04:11 +0800

    add db project

---
+++ DbFirstExample.Database/dbo/Tables/Job.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Job] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [Title]             NVARCHAR (MAX) NOT NULL,
+    [Item_WorkItemId]   INT            NOT NULL,
+    [Item_Description]  NVARCHAR (MAX) NOT NULL,
+    [Item_WorkTypeId]   INT            NOT NULL,
+    [Item_WorkTypeName] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Job] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ DbFirstExample.Database/dbo/Tables/WorkItem.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[WorkItem] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [WorkTypeId]  INT            NOT NULL,
+    CONSTRAINT [PK_WorkItem] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_WorkItem_WorkType_WorkTypeId] FOREIGN KEY ([WorkTypeId]) REFERENCES [dbo].[WorkType] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_WorkItem_WorkTypeId]
+    ON [dbo].[WorkItem]([WorkTypeId] ASC);
+
